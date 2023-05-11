645ae2a846665311159c7ce338deb1ff4b3db799
Administrator <velikkom@hotmail.com>
2023-05-11 14:01:45 +0300

    day 4 01:45

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     baseUrl : 'https://practice.cydeo.com',
     video:false,
     retries:1,
+    defaultCommandTimeout:5000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/buttons.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+
+describe("find or get elements by using different locators ", () => {
+
+    beforeEach(() => {
+        //run before each test cases ,before Method in TestNG
+        cy.clearCookies();
+        cy.visit('/multiple_buttons');
+    })
+
+
+    //open cypress|set ".only"
+    it('Check Different Button Actions', () => {
+        //select a button text
+        cy.contains('Button 2').should('be.visible').click();
+        cy.contains('Clicked on button two!').should('be.visible');
+    })
+})
