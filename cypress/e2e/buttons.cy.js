7e7ea34dca74456101a26cd55f1f73e560e4f634
Administrator <velikkom@hotmail.com>
2023-05-29 14:49:28 +0300

    29.05.2023 day 4 free working

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -12,5 +12,11 @@ describe('find or get elements by using different locators ', () => {
     // select a button text
     cy.contains('Button 2').should('be.visible').click();
     cy.contains('Clicked on button two!').should('be.visible');
+    //find element with class attribute and create a list then select 3rd element
+    cy.get('.btn.btn-primary').then(($buttons)=>{
+      cy.wrap($buttons).eq(1).click();//list buttons element and click index 1 element
+      //assert the text
+      cy.contains('Clicked on button two!').should('be.visible');
+    })
   });
 });
