db4b578d1ea72360a34a8abc869c23cc7b6b7c02
Administrator <velikkom@hotmail.com>
2023-05-29 18:38:02 +0300

    29.05.2023 day 4 free working

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -18,5 +18,22 @@ describe('find or get elements by using different locators ', () => {
       //assert the text
       cy.contains('Clicked on button two!').should('be.visible');
     })
+    cy.get('button').each((item,index,list)=>{
+      expect(list).to.have.length(6);
+      expect(item).to.have.attr("onclick");
+    })
+    // I will gett all buttons like previous approach get only the item then check for
+    //text of each item if its equal to button 4 then click on it
+
+    cy.get('button').each((item)=>{
+      if (item.text()==='Button 4'){
+        cy.log(item.text());//write the text console
+       // item.click(); we can not use cypress click func on jquery element
+        cy.wrap(item).click();
+        cy.contains('Clicked on button four!').should('be.visible');
+      }
+    })
+
+
   });
 });
